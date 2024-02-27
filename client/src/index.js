6756b010700b62610e3a4c5dbe8755eb35fe953a
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-02-27 15:33:08 -0600

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './utils/api';
-import { AuthProvider } from './utils/auth'; // Import AuthProvider
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AuthProvider> {/* Wrap your App component with AuthProvider */}
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
